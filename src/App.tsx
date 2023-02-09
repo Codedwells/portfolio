9fcda7311eeef60992a2fb8d4a65f9e9b65cf11b
coddedwells <wells.mail.13@gmail.com>
2023-02-09 19:25:24 +0300

    📦 NEW: Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+const App = () => {
+	const [count, setCount] = useState(0);
+
+	return (
+		<>
+			<p className=' bg-orange-600'>Welcome me Universe</p>
+		</>
+	);
+};
+
+export default App;
