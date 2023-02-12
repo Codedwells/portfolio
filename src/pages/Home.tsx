bfc72cd5af14e6a8602537d6cd219add6ed7030e
coddedwells <wells.mail.13@gmail.com>
2023-02-12 23:06:22 +0300

    📦 NEW: favicon added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,8 +7,8 @@ const Home = () => {
 	return (
 		<>
 			<section className='flex items-center justify-center flex-col-reverse lg:flex-row  md:space-x-12'>
-				<div className='flex flex-col mt-8 md:pl-20 md:mt-20 lg:pl-0'>
-					<p className='font-[700] px-4 md:px-0 text-left text-[3rem]  md:text-[4rem] max-w-[500px] whitespace-normal leading-[4rem]'>
+				<div className='flex flex-col text-[#1f2045] mt-8 md:pl-20 md:mt-20 lg:pl-0'>
+					<p className='font-[700] max-w-[400px] px-4 md:px-0 text-left text-[3rem]  md:text-[4rem] md:max-w-[500px] whitespace-normal leading-[4rem]'>
 						Hello there, thanks for{' '}
 						<span className='text-[#0188f7]'>visiting</span> 👋.&nbsp; I'm Abel.
 					</p>
