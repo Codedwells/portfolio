6e3fe8c8e2434a201da99d396784b89f9a804750
Codedwells <wells.mail.13@gmail.com>
2024-02-02 15:51:44 +0300

    chore(SEO): Improved SEO

--- v2/src/app/layout.tsx
+++ v2/src/app/layout.tsx
@@ -7,7 +7,28 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
 	title: 'Abel Misiocha',
-	description: "Abel Misiocha's personal website"
+	description: "Abel Misiocha's personal website",
+	metadataBase: new URL('https://abeldev.site'),
+	openGraph: {
+		type: 'website',
+		locale: 'en_US',
+		url: '/',
+		images: '/opengraph-image.webp'
+	},
+	robots: {
+		index: false,
+		follow: true,
+		nocache: true,
+		googleBot: {
+			index: true,
+			follow: false,
+			noimageindex: true,
+			'max-video-preview': -1,
+			'max-image-preview': 'large',
+			'max-snippet': -1
+		}
+	},
+	category: 'Personal Website',
 }
 
 export default function RootLayout({
